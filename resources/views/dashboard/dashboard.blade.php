064b375f95f2e8ea87f97cf32e7cd79af5b9aa60
Ntagawa Innocent <ntagawa0705@gmail.com>
2022-10-07 17:46:11 +0200

    create layout

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.base')
+
+@section('main')
+<div class="container-fluid">
+    <!-- ============================================================== -->
+                <!-- pagehader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="page-header">
+                            <h2 class="pageheader-title"><i class="fa fa-fw fa-tachometer-alt"></i> Dashboard </h2>
+                            <div class="page-breadcrumb">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Home</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- pagehader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <!-- metric -->
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-inline-block">
+                                    <h5 class="text-muted">Number of students</h5>
+                                    <h2 class="mb-0"> 150</h2>
+                                </div>
+                                <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
+                                    <i class="fa fa-user-graduate fa-fw fa-sm text-info"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /. metric -->
+                    <!-- metric -->
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-inline-block">
+                                    <h5 class="text-muted">Number of total request</h5>
+                                    <h2 class="mb-0"> 16</h2>
+                                </div>
+                                <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
+                                    <i class="fa fa-eye fa-fw fa-sm text-info"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /. metric -->
+                    <!-- metric -->
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-inline-block">
+                                    <h5 class="text-muted">Number of pending request</h5>
+                                    <h2 class="mb-0"> 26</h2>
+                                </div>
+                                <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
+                                    <i class="fa fa-eye fa-fw fa-sm text-info"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /. metric -->
+                    <!-- metric -->
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-inline-block">
+                                    <h5 class="text-muted">Number of completed request</h5>
+                                    <h2 class="mb-0"> 156</h2>
+                                </div>
+                                <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
+                                    <i class="fa fa-eye fa-fw fa-sm text-info"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /. metric -->
+
+</div>
+@endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -31,107 +31,17 @@
     <!-- main wrapper -->
     <!-- ============================================================== -->
     <div class="dashboard-main-wrapper">
-        <!-- ============================================================== -->
-        <!-- navbar -->
-        <!-- ============================================================== -->
-        <div class="dashboard-header">
-            <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                <a class="navbar-brand" href="index.html" style="font-family: Impact , sans-serif;">Online-Documents-Requesting-System</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto navbar-right-top">
-                        <li class="nav-item">
-                            <div id="custom-search" class="top-search-bar">
-                                <input class="form-control" type="text" placeholder="Search..">
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown nav-user">
-                            <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="../assets/images/avatar.jpg" alt="" class="user-avatar-md rounded-circle"></a>
-                            <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
-                                <div class="nav-user-info" style="background-color: rgb(255, 55, 0);">
-                                    <h5 class="mb-0 text-white nav-user-name">Ntagawa I </h5>
-                                    <span class="status"></span><span class="ml-2">Administrator</span>
-                                </div>
-                                <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
-                                <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a>
-                                <a class="dropdown-item" href="../index.html"><i class="fas fa-power-off mr-2"></i>Logout</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-        <!-- ============================================================== -->
-        <!-- end navbar -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- left sidebar -->
-        <!-- ============================================================== -->
-        <div class="nav-left-sidebar sidebar-dark" style="background-color: rgb(39, 44, 74);">
-            <div class="menu-list">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav flex-column">
-                            <li class="nav-divider">
-                                <a href="index.html"><img class="logo-img" src="../assets/images/odrs logo.png" alt="logo"></a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="index.html" style="background-color: rgb(27, 31, 61);"><i class="fa fa-fw fa-tachometer-alt"></i>Dashboard <span class="badge badge-success">6</span></a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="course.html"><i class="fa fa-fw fa-certificate"></i>Course <span class="badge badge-success">6</span></a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="student.html"><i class="fa fa-fw fa-user-graduate"></i>Student <span class="badge badge-success">6</span></a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="document.html"><i class="fa fa-fw fa-file-word"></i>Document <span class="badge badge-success">6</span></a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="request.html"><i class="fa fa-fw fa-file"></i>Request <span class="badge badge-success">6</span></a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="payment.html"><i class="fa fa-fw fa-money-bill-wave"></i>Payment <span class="badge badge-success">6</span></a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="users.html"><i class="fa fa-fw fa-user-lock"></i>User <span class="badge badge-success">6</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-8" aria-controls="submenu-8"><i class="fas fa-fw fa-chart-pie"></i>Reports</a>
-                                <div id="submenu-8" class="collapse submenu">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="income-report.html">Income</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="course-report.html">Request by Course</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="status-report.html">Request Status</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
-        <!-- ============================================================== -->
-        <!-- end left sidebar -->
-        <!-- ============================================================== -->
+        @include('layouts._top-nav')
+        @include('layouts._side-bar')
         <!-- ============================================================== -->
         <!-- wrapper  -->
         <!-- ============================================================== -->
         <div class="dashboard-wrapper">
-            @yield('main')
+            <div class="container-fluid dashboard-content">
+                @yield('main')
+            </div>
         </div>
+    </div>
         <!-- ============================================================== -->
         <!-- end main wrapper  -->
         <!-- ============================================================== -->
